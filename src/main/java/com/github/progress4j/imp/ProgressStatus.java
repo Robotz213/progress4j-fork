3bf18c5ad6f98c0f78dd1022661c7f0b6fb478e3
Leonardo de Lima Oliveira <leoolive@trf3.jus.br>
2022-07-22 16:56:13 -0400

    progress status (for downloading)

---
+++ src/main/java/com/github/progress4j/imp/ProgressStatus.java
@@ -0,0 +1,62 @@
+package com.github.progress4j.imp;
+
+import java.io.File;
+
+import com.github.progress4j.IProgress;
+import com.github.progress4j.IStage;
+import com.github.utils4j.imp.Args;
+import com.github.utils4j.imp.DownloadStatus;
+
+public class ProgressStatus extends DownloadStatus {
+  
+  private long total;  
+  
+  private int increment = 1;  
+  
+  private final IStage stage;  
+  
+  private final IProgress progress;
+
+  public ProgressStatus(IProgress progress, IStage stage) {
+    this(progress, stage, true, null);
+  }
+
+  public ProgressStatus(IProgress progress, IStage stage, File saveHere) {
+    this(progress, stage, true, saveHere);
+  }
+    
+  public ProgressStatus(IProgress progress, IStage stage, boolean rejectEmpty, File saveHere) {
+    super(rejectEmpty, saveHere);
+    this.stage = Args.requireNonNull(stage, "stage is null");
+    this.progress = Args.requireNonNull(progress, "progress is null");
+  }  
+  
+  @Override
+  protected void onStepStart(long total) throws InterruptedException {
+    this.total = total;
+    progress.begin(stage, 100);
+  }
+  
+  @Override
+  protected void onStepEnd() throws InterruptedException {
+    progress.end();
+  }
+  
+  @Override
+  protected void onStepFail(Throwable e) throws InterruptedException {
+    onStepEnd();
+  }
+  
+  @Override
+  protected void onStepStatus(long written) throws InterruptedException { 
+    float percent = 100f * written / total;
+    if (percent >= increment) {
+      progress.step("Baixados %d%%", increment++);
+    }
+    if (increment <= percent) {  // <= or < ?
+      long diff = (long)(percent - increment + 1);
+      progress.skip(diff);
+      increment += diff;
+    }
+  } 
+}
